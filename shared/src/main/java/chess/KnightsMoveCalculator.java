e9187745cb323dd65f9882d68ed63b2930a84e2b
Ryan Kane <rtkane@byu.edu>
2024-05-08 00:00:07 -0600

    knight moves passed

--- shared/src/main/java/chess/KnightsMoveCalculator.java
+++ shared/src/main/java/chess/KnightsMoveCalculator.java
@@ -5,12 +5,63 @@ import java.util.Collection;
 
 public class KnightsMoveCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position){
-        int currRow = position.getRow();
-        int currCol = position.getColumn();
+        int currRow;
+        int currCol;
 
         Collection<ChessMove> validMoves = new ArrayList<>();
 
-
+        for(int i = 0; i < 8; i++){
+            currRow = position.getRow();
+            currCol = position.getColumn();
+            if (i == 0){
+                currRow += 2;
+                currCol += 1;
+                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                validMoves.add(move);
+            }
+            if (i == 1){
+                currRow += 1;
+                currCol += 2;
+                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                validMoves.add(move);
+            }
+            if (i == 2){
+                currRow -= 1;
+                currCol += 2;
+                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                validMoves.add(move);
+            }
+            if (i == 3){
+                currRow -= 2;
+                currCol += 1;
+                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                validMoves.add(move);
+            }
+            if (i == 4){
+                currRow -= 2;
+                currCol -= 1;
+                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                validMoves.add(move);
+            }
+            if (i == 5){
+                currRow -= 1;
+                currCol -= 2;
+                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                validMoves.add(move);
+            }
+            if (i == 6){
+                currRow += 1;
+                currCol -= 2;
+                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                validMoves.add(move);
+            }
+            if (i == 7){
+                currRow += 2;
+                currCol -= 1;
+                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                validMoves.add(move);
+            }
+        }
 
         return validMoves;
     }
