022a18c2951f226e22385261c0b6ce45b19c1c6d
Ryan Kane <rtkane@byu.edu>
2024-05-09 13:28:16 -0600

    all queen tests passed

--- shared/src/main/java/chess/QueenMoveCalculator.java
+++ shared/src/main/java/chess/QueenMoveCalculator.java
@@ -16,61 +16,213 @@ public class QueenMoveCalculator {
             if (i == 0){
                 while (currRow <= 7){
                     currRow +=1;
-                    ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
-                    validMoves.add(move);
+                    if (inBounds(new ChessPosition(currRow, currCol))) {
+                        if (isSpaceEmpty(board, new ChessPosition(currRow, currCol))) {
+                            ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+
+                            validMoves.add(move);
+                        }
+                        else {
+                            if (checkTeamColor(board,new ChessPosition(currRow, currCol), position)){
+                                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                                validMoves.add(move);
+                                break;
+                            }
+                            else {
+                                break;
+                            }
+
+                        }
+                    }
+                    else {
+                        break;
+                    }
                 }
             }
             if (i == 1){
                 while (currRow <= 7 && currCol <= 7){
                     currRow +=1;
                     currCol +=1;
-                    ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
-                    validMoves.add(move);
+                    if (inBounds(new ChessPosition(currRow, currCol))) {
+                        if (isSpaceEmpty(board, new ChessPosition(currRow, currCol))) {
+                            ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+
+                            validMoves.add(move);
+                        }
+                        else {
+                            if (checkTeamColor(board,new ChessPosition(currRow, currCol), position)){
+                                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                                validMoves.add(move);
+                                break;
+                            }
+                            else {
+                                break;
+                            }
+
+                        }
+                    }
+                    else {
+                        break;
+                    }
                 }
             }
             if (i == 2){
                 while (currCol <= 7){
                     currCol +=1;
-                    ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
-                    validMoves.add(move);
+                    if (inBounds(new ChessPosition(currRow, currCol))) {
+                        if (isSpaceEmpty(board, new ChessPosition(currRow, currCol))) {
+                            ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+
+                            validMoves.add(move);
+                        }
+                        else {
+                            if (checkTeamColor(board,new ChessPosition(currRow, currCol), position)){
+                                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                                validMoves.add(move);
+                                break;
+                            }
+                            else {
+                                break;
+                            }
+
+                        }
+                    }
+                    else {
+                        break;
+                    }
                 }
             }
             if (i == 3){
                 while (currRow > 1 && currCol <= 7){
                     currRow -=1;
                     currCol +=1;
-                    ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
-                    validMoves.add(move);
+                    if (inBounds(new ChessPosition(currRow, currCol))) {
+                        if (isSpaceEmpty(board, new ChessPosition(currRow, currCol))) {
+                            ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+
+                            validMoves.add(move);
+                        }
+                        else {
+                            if (checkTeamColor(board,new ChessPosition(currRow, currCol), position)){
+                                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                                validMoves.add(move);
+                                break;
+                            }
+                            else {
+                                break;
+                            }
+
+                        }
+                    }
+                    else {
+                        break;
+                    }
                 }
             }
             if (i == 4){
                 while (currRow > 1){
                     currRow -=1;
-                    ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
-                    validMoves.add(move);
+                    if (inBounds(new ChessPosition(currRow, currCol))) {
+                        if (isSpaceEmpty(board, new ChessPosition(currRow, currCol))) {
+                            ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+
+                            validMoves.add(move);
+                        }
+                        else {
+                            if (checkTeamColor(board,new ChessPosition(currRow, currCol), position)){
+                                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                                validMoves.add(move);
+                                break;
+                            }
+                            else {
+                                break;
+                            }
+
+                        }
+                    }
+                    else {
+                        break;
+                    }
                 }
             }
             if (i == 5){
                 while (currRow > 1 && currCol > 1){
                     currRow -=1;
                     currCol -=1;
-                    ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
-                    validMoves.add(move);
+                    if (inBounds(new ChessPosition(currRow, currCol))) {
+                        if (isSpaceEmpty(board, new ChessPosition(currRow, currCol))) {
+                            ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+
+                            validMoves.add(move);
+                        }
+                        else {
+                            if (checkTeamColor(board,new ChessPosition(currRow, currCol), position)){
+                                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                                validMoves.add(move);
+                                break;
+                            }
+                            else {
+                                break;
+                            }
+
+                        }
+                    }
+                    else {
+                        break;
+                    }
                 }
             }
             if (i == 6){
                 while (currCol > 1){
                     currCol -=1;
-                    ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
-                    validMoves.add(move);
+                    if (inBounds(new ChessPosition(currRow, currCol))) {
+                        if (isSpaceEmpty(board, new ChessPosition(currRow, currCol))) {
+                            ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+
+                            validMoves.add(move);
+                        }
+                        else {
+                            if (checkTeamColor(board,new ChessPosition(currRow, currCol), position)){
+                                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                                validMoves.add(move);
+                                break;
+                            }
+                            else {
+                                break;
+                            }
+
+                        }
+                    }
+                    else {
+                        break;
+                    }
                 }
             }
             if (i == 7){
                 while (currRow <= 7 && currCol > 1){
                     currRow +=1;
                     currCol -=1;
-                    ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
-                    validMoves.add(move);
+                    if (inBounds(new ChessPosition(currRow, currCol))) {
+                        if (isSpaceEmpty(board, new ChessPosition(currRow, currCol))) {
+                            ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+
+                            validMoves.add(move);
+                        }
+                        else {
+                            if (checkTeamColor(board,new ChessPosition(currRow, currCol), position)){
+                                ChessMove move = new ChessMove(position, new ChessPosition(currRow, currCol), null);
+                                validMoves.add(move);
+                                break;
+                            }
+                            else {
+                                break;
+                            }
+
+                        }
+                    }
+                    else {
+                        break;
+                    }
                 }
             }
 
@@ -80,5 +232,43 @@ public class QueenMoveCalculator {
         }
 
         return validMoves;
+    }
+
+
+
+    private Boolean inBounds(ChessPosition position){
+        if (position.getRow() > 8){
+            return false;
+        }
+        if (position.getColumn() > 8){
+            return false;
+        }
+        if (position.getRow() < 1){
+            return false;
+        }
+        if (position.getColumn() < 1){
+            return false;
+        }
+        return true;
+    }
+
+    private Boolean isSpaceEmpty(ChessBoard board, ChessPosition position){
+
+        if (board.getPiece(position) == null){
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean checkTeamColor(ChessBoard board, ChessPosition newPosition, ChessPosition position){
+        if (board.getPiece(newPosition).getTeamColor().equals(ChessGame.TeamColor.WHITE) == board.getPiece(position).getTeamColor().equals(ChessGame.TeamColor.WHITE)){
+            return false;
+        }
+        if (board.getPiece(newPosition).getTeamColor().equals(ChessGame.TeamColor.BLACK) == board.getPiece(position).getTeamColor().equals(ChessGame.TeamColor.BLACK)){
+            return false;
+        }
+
+
+        return true;
     }
 }
